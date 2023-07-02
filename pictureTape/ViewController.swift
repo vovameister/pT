8b9af26c6cfb637ed88771b8e04831e700821e30
vovameister <zonda4242@gmail.com>
2023-07-02 19:48:03 +0200

    Initial Commit

---
+++ pictureTape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pictureTape
+//
+//  Created by Владимир Клевцов on 2.7.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
